2c6978c0dfb40487ba1eee2ee80e36764a9ea963
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2025-12-31 16:46:12 +1100

    31/12/25: Enemy: Death System
    Added simple death functionality

--- idle-clicker-poker/Scripts/CharacterScripts/EnemyBase.cs
+++ idle-clicker-poker/Scripts/CharacterScripts/EnemyBase.cs
@@ -3,36 +3,65 @@ using System;
 
 public partial class EnemyBase : Area2D
 {
-    [Export] private float health = 50;
     private Node playerObject;
+    private Player playerScript;
+
+    private Sprite2D sprite;
+    private int animFrame = 0;
+
+    [Export] private float health = 20;
     [Export] public float damage = 10;
+
+    private bool isDead;
 
     // This function is called once when the node is in the scene and is ready to be used
     public override void _Ready()
     {
         base._Ready();
-        SetPlayerObject();
+        GetPlayer();
+        GetSprite();
     }
 
     // Gets and stores the Player node
     // Might replace this once there is a Manager/Spawner/Singleton for enemies
-    private void SetPlayerObject()
+    private void GetPlayer()
     {
         // Get the Player node which stores the player related data
-        playerObject = GetNode("../Player");
+        playerObject = GetNode("../PlayerObject");
+        playerScript = GetNode<Player>("../PlayerObject");
 
         // Test case to ensure we are getting the right Node
         if (playerObject != null) GD.Print("Sucessfully found " + playerObject.Name); 
         else GD.Print("Get Node Failed!");
     }
 
-	public void TakeDamage(float _damage){
-		health -= _damage;
+    // Gets and stores the Sprite node
+    private void GetSprite()
+    {
+        sprite = GetChild<Sprite2D>(1);
+
+        if (sprite != null) GD.Print("Sucessfully found " + sprite.Name);
+        else GD.Print("Get Sprite Failed!");
+    }
+
+    public void TakeDamage(float _damage){
+		health -= playerScript.GetDamage();
         GD.Print("Ow! Health: " + health);
+
+        if (health <= 0) Die();
+    }
+
+    private void Die()
+    {
+        health = 0;
+
+        sprite.Frame = 1;
+        isDead = true;
     }
 
     public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
     {
+        if (isDead) return;
         base._InputEvent(viewport, @event, shapeIdx);
         // If the Character is clicked then interact with it
 		if (Input.IsActionPressed("Click")) TakeDamage(damage);
