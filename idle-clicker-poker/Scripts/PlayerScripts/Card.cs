728988b71a9a10b07d4e99d3fe42f05ceef2a208
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2026-01-11 17:24:07 +1100

    11/01/26: Art: Added the rest of the card sprites

--- idle-clicker-poker/Scripts/PlayerScripts/Card.cs
+++ idle-clicker-poker/Scripts/PlayerScripts/Card.cs
@@ -5,8 +5,28 @@ using static System.Net.Mime.MediaTypeNames;
 
 public partial class Card : Sprite2D
 {
+    private static int SPRITE_WIDTH = 63;
+    private static int SPRITE_HEIGHT = 88;
+
+    private int totalCardSprites;
+
     private int numberID;
     private int suitID;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        GetSpriteCount();
+    }
+
+    private void GetSpriteCount()
+    {
+        float x = Texture.GetWidth() / SPRITE_WIDTH;
+        float y = Texture.GetHeight() / SPRITE_HEIGHT;
+
+        totalCardSprites = (int)x * (int)y;
+    }
 
     public override void _Input(InputEvent @event)
     {
@@ -18,8 +38,8 @@ public partial class Card : Sprite2D
 
         if (numberID < 0)
             numberID = 0;
-        else if (numberID > 12)
-            numberID = 12;
+        else if (numberID > totalCardSprites - 1)
+            numberID = totalCardSprites - 1;
 
         UpdateSprite();
     }
